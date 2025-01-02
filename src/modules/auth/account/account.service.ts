5a592122caa042e06ca4b3877ce21733500b100e
Nikita Kopylov <nik.kopylov.8080@mail.ru>
2025-01-02 23:39:18 +0300

    feat: create account

--- src/modules/auth/account/account.service.ts
+++ src/modules/auth/account/account.service.ts
@@ -1,6 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
+import { hash } from 'argon2';
 
 import { PrismaService } from '@/src/core/prisma/prisma.service';
+
+import { CreateUserInput } from './inputs/create-user.input';
 
 @Injectable()
 export class AccountService {
@@ -10,5 +13,40 @@ export class AccountService {
     const users = await this.prismaService.user.findMany();
 
     return users;
+  }
+
+  public async create(input: CreateUserInput) {
+    const { username, email, password } = input;
+
+    const isUsernameExists = await this.prismaService.user.findUnique({
+      where: {
+        username,
+      },
+    });
+
+    if (isUsernameExists) {
+      throw new ConflictException('Это имя пользователя уже занято');
+    }
+
+    const isEmailExists = await this.prismaService.user.findUnique({
+      where: {
+        email,
+      },
+    });
+
+    if (isEmailExists) {
+      throw new ConflictException('Эта почта уже занята');
+    }
+
+    await this.prismaService.user.create({
+      data: {
+        username,
+        email,
+        password: await hash(password),
+        displayName: username,
+      },
+    });
+
+    return true;
   }
 }
