552f0531b65e9d2b5f974fba27ea97bef9c3385c
Nikita Kopylov <nik.kopylov.8080@mail.ru>
2025-01-31 00:52:10 +0300

    feat: email verification

--- src/modules/auth/account/account.service.ts
+++ src/modules/auth/account/account.service.ts
@@ -3,11 +3,16 @@ import { hash } from 'argon2';
 
 import { PrismaService } from '@/src/core/prisma/prisma.service';
 
+import { VerificationService } from '../verification/verification.service';
+
 import { CreateUserInput } from './inputs/create-user.input';
 
 @Injectable()
 export class AccountService {
-  public constructor(private readonly prismaService: PrismaService) {}
+  public constructor(
+    private readonly prismaService: PrismaService,
+    private readonly verificationService: VerificationService,
+  ) {}
 
   public async me(id: string) {
     const user = await this.prismaService.user.findUnique({
@@ -40,7 +45,7 @@ export class AccountService {
       throw new ConflictException('Эта почта уже занята');
     }
 
-    await this.prismaService.user.create({
+    const user = await this.prismaService.user.create({
       data: {
         username,
         email,
@@ -48,6 +53,8 @@ export class AccountService {
         displayName: username,
       },
     });
+
+    await this.verificationService.sendVerificationToken(user);
 
     return true;
   }
